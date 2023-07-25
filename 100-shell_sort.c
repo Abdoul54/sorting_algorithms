ff80d575ce3a666afc648bd830773a8531f540e2
Abdoul54 <abdelwahed.akhechane@gmail.com>
2023-07-25 23:07:38 +0100

    FIX IT RALPH

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,39 +1,41 @@
 #include "sort.h"
 
 /**
- * shell_sort -> Sorts an array of integers in ascending order
- * @array: Array Integers
- * @size: Array's size
- **/
+*swap - the positions of two elements into an array
+*@array: array
+*@item1: array element
+*@item2: array element
+*/
+void swap(int *array, int item1, int item2)
+{
 
+	int tmp;
+
+	tmp = array[item1];
+	array[item1] = array[item2];
+	array[item2] = tmp;
+}
+/**
+ * shell_sort - function that sorts an array of integers in ascending
+ * order using the Shell sort algorithm, using the Knuth sequence
+ * @size: size of the array
+ * @array: list with numbers
+ */
 void shell_sort(int *array, size_t size)
 {
-	size_t kun[1000], k = 0, j = 0, i;
-	int n, k;
+	size_t gap = 1, i, index = 0;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
-	while (j * 3 + 1 < size)
+	while (gap < size / 3)
+		gap = 3 * gap + 1;
+	while (gap >= 1)
 	{
-		kun[k] = j * 3 + 1;
-		j = kun[k++];
-	}
-	for (i = 0; i < k; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			if ((j + kun[k - i - 1]) > size - 1)
-				break;
-			k = j;
-			while (array[k] > array[k + kun[k - i - 1]])
-			{
-				n = array[k];
-				array[k] =  array[k + kun[k - i - 1]];
-				array[k + kun[k - i - 1]] = n;
-				k = k - kun[k - i - 1];
-				if (k < 0)
-					break;
-			}
-		} print_array(array, size);
+		for (i = gap; i < size; i++)
+			for (index = i; index >= gap &&
+			 (array[index] < array[index - gap]); index -= gap)
+				swap(array, index, index - gap);
+		print_array(array, size);
+		gap /= 3;
 	}
 }
